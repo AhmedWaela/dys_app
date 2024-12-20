99ac4e527d0294c172da752aec3ed0a10af39916
ahmedwael <ahmedwael99104@gmail.com>
2024-12-20 20:55:51 +0200

    create add reminder view app bar

--- lib/features/tasks/presentation/views/add_reminder_view.dart
+++ lib/features/tasks/presentation/views/add_reminder_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sanad_app/features/tasks/presentation/views/widgets/custom_app_bar.dart';
 
 class AddReminderView extends StatelessWidget {
   const AddReminderView({super.key});
   static const String route = '/addreminderview';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      appBar: CustomAppBar(
+        title: 'Add Reminder',
+      ),
+    );
   }
 }
