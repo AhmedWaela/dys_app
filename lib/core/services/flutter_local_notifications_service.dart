e0287983dac222c53e1422b9eee1c262b94f1c8e
ahmedwael <ahmedwael99104@gmail.com>
2024-12-20 22:24:33 +0200

    create show basic notification  method

--- lib/core/services/flutter_local_notifications_service.dart
+++ lib/core/services/flutter_local_notifications_service.dart
@@ -27,4 +27,28 @@ class FlutterLocalNotificationsService {
   static void onDidReceiveBackgroundNotificationResponse(details) {}
 
   static void onDidReceiveNotificationResponse(details) {}
+
+  static Future<void> showBasicNotification(
+    int id,
+    String title,
+    String description,
+  ) async {
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+          'Basic Notification Channel Id',
+          'BasicNotification Channel Name',
+          sound: RawResourceAndroidNotificationSound('basic_notification'),
+          subText: 'Be ready for task',
+          actions: [
+            AndroidNotificationAction(
+              'id',
+              'Clear',
+              cancelNotification: true,
+            )
+          ],
+        ),
+        iOS: DarwinNotificationDetails());
+    await flutterLocalNotificationsPlugin.show(
+        id, title, description, notificationDetails);
+  }
 }
