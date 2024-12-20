c29675c3f48cd260a2e01b63e3700e7254df7d4c
ahmedwael <ahmedwael99104@gmail.com>
2024-12-20 20:59:00 +0200

    create custom text field

--- lib/features/tasks/presentation/views/widgets/add_reminder_view_body.dart
+++ lib/features/tasks/presentation/views/widgets/add_reminder_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sanad_app/features/tasks/presentation/views/widgets/custom_text_field.dart';
 
 class AddReminderViewBody extends StatelessWidget {
   const AddReminderViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return const Column(
+      children: [
+        CustomTextField(),
+        CustomTextField(),
+      ],
+    );
   }
 }
