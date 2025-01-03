f531d65eb31e8917fe7ad57439195e96e82dcfce
ahmedwael <ahmedwael99104@gmail.com>
2025-01-03 16:42:33 +0200

    create ball and its shadow

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:dys_app/core/utils/app_dimentions.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -6,8 +7,40 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      height: AppDimentions.screenHight,
+      width: AppDimentions.screenWidth,
       child: Stack(
-        children: [],
+        alignment: Alignment.center,
+        children: [
+          AnimatedPositioned(
+            bottom: 500,
+            duration: const Duration(milliseconds: 600),
+            child: Column(
+              children: [
+                Transform.translate(
+                  offset: const Offset(0, 50),
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 1000),
+                    width: 50,
+                    height: 50,
+                    decoration: const BoxDecoration(
+                      color: Color(0xff4CB050),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                if (true)
+                  Container(
+                    width: 50,
+                    height: 10,
+                    decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(100)),
+                  ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
