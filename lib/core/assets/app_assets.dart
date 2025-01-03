84f0c2849a9ace04a800a53af1e3436c5c3a5292
ahmedwael <ahmedwael99104@gmail.com>
2025-01-03 19:44:24 +0200

    create class contains assets paths

---
+++ lib/core/assets/app_assets.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static const String onboarding1 = "assets/images/onboarding1.json";
+  static const String onboarding2 = "assets/images/onboarding1.json";
+  static const String onboarding3 = "assets/images/onboarding1.json";
+}
--- lib/features/onboarding/presentation/views/widgets/onboarding_item.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_item.dart
@@ -1,12 +1,15 @@
 import 'package:dys_app/core/exports/exports.dart';
+import 'package:lottie/lottie.dart';
 
 class OnBoardingItem extends StatelessWidget {
   const OnBoardingItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      children: [
+        // Lottie.asset("")
+      ],
     );
   }
 }
