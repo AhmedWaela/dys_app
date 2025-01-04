440f40527aa158aa623b26d317aa71281e86e85f
ahmedwael <ahmedwael99104@gmail.com>
2025-01-04 14:41:50 +0200

    create sign up view body

---
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:dys_app/core/exports/exports.dart';
+
+class SignUpViewBody extends StatelessWidget {
+  const SignUpViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView();
+  }
+}
