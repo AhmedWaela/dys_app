cfc6f25bbb428af87a0bfc12a2d85a77e2e71f16
ahmedwael <ahmedwael99104@gmail.com>
2025-01-05 12:27:36 +0200

    add firebase core package

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -8,29 +8,78 @@ class SignUpViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: AppDimentions.screenHight * 20 / 812,
-            ),
-            SvgPicture.asset(AppAssets.signUpSvg),
-            SizedBox(
-              height: AppDimentions.screenHight * 24 / 812,
-            ),
-            const Text(
-              "Start your Journy with us",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: AppDimentions.screenHight * 20 / 812,
+              ),
+              SvgPicture.asset(AppAssets.signUpSvg),
+              SizedBox(
+                height: AppDimentions.screenHight * 24 / 812,
+              ),
+              const Text(
+                "Start your Journy with us",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: AppDimentions.screenHight * 32 / 812,
+              ),
+              AuthTextFormField()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AuthTextFormField extends StatelessWidget {
+  const AuthTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      decoration: BoxDecoration(
+        color: Color(0xffF9FFFA),
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0x29000000),
+            blurRadius: 5.2,
+            offset: Offset(0, -1),
+          ),
+        ],
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+            hintText: "Email",
+            prefixIcon: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: SvgPicture.asset(
+                "assets/images/email.svg",
+                width: 24,
+                height: 10,
+                colorFilter:
+                    ColorFilter.mode(Color(0xff6D6D6D), BlendMode.srcIn),
               ),
             ),
-            SizedBox(
-              height: AppDimentions.screenHight * 32 / 812,
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
             ),
-          ],
-        ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            )),
       ),
     );
   }
